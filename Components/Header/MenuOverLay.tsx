68082ccfdbd585672d2c0fb1a9e9277521afd2fe
FarhanaIslamSaima <farhanachowdhury2152001@gmail.com>
2024-01-13 00:15:10 +0600

    updated

--- Components/Header/MenuOverLay.tsx
+++ Components/Header/MenuOverLay.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useState } from 'react';
 import React from 'react';
 import NavLinks from './NavLinks';
 interface prop{
@@ -8,26 +10,45 @@ interface props{
     link:prop
 }
 const navLinks = [
-    {
+  { id:1,
+    title: "Home",
+    path: "/",
+  },
+    { id:2,
       title: "About",
-      path: "#about",
+      path: "#motto",
     },
-    {
-      title: "Projects",
-      path: "#projects",
+    { id:3,
+      title: "Events",
+      path: "/Events",
+      year:[2017,2018,2019,2020,2021,2022,2023]
     },
-    {
+    
+    { id:4,
+      title: "Blogs",
+      path: "/Blogs",
+    },
+    { id:5,
+      title: "Executives",
+      path: "/panel",
+    },
+    { id:6,
+      title: "Developers",
+      path: "/devs",
+    },
+    { id:7,
       title: "Contact",
-      path: "#contact",
+      path: "#footer",
     },
   ];
 
 const MenuOverLay = () => {
+  const [active,setActive]=useState(1);
     return (
         <ul className="flex xs:bg-black  bg-black flex-col py-4 items-center">
         {navLinks.map((link, index) => (
           <li key={index}>
-            <NavLinks link={link} />
+            <NavLinks link={link} active={active}  />
           </li>
         ))}
       </ul>
