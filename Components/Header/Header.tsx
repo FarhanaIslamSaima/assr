9b1313e482c0be2f9e1f9fa5ef906b864fabb396
FarhanaIslamSaima <farhanachowdhury2152001@gmail.com>
2024-03-07 13:41:12 +0600

    updated

--- Components/Header/Header.tsx
+++ Components/Header/Header.tsx
@@ -6,6 +6,7 @@ import MenuOverLay from './MenuOverLay';
 import { link } from 'fs';
 import { GiHamburgerMenu } from "react-icons/gi";
 import { ImCross } from "react-icons/im";
+import { IoIosArrowDown } from "react-icons/io";
 
 const navLinks = [
   { id:1,
@@ -18,7 +19,7 @@ const navLinks = [
     },
     { id:3,
       title: "Events",
-      path: "/Events",
+      path: "#",
       year:[2017,2018,2019,2020,2021,2022,2023]
     },
     
@@ -41,6 +42,9 @@ const navLinks = [
   ];
   
 const Header = () => {
+  const [navbar, setNavbar] = useState(false);
+  
+  const cat=['Politics','Education','Agriculture','Sports']
   const [open,setOpen]=useState(false)
   const [active,setActive]=useState(1);
     const toogle=()=>{
@@ -51,62 +55,125 @@ const Header = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
     return (
         
-            
-        <nav className="absolute mx-auto top-0 left-0 right-0 z-10 bg-opacity-100  ">
-        <div className="flex container  flex-wrap items-center justify-between mx-auto px-4 py-1">
-          <Link
-            href={"/"}
-            className="text-2xl md:text-5xl text-white font-semibold"
-          >
-            <h1 className="text-white">
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-red-700">
-                <img src="/logo.png" width={'110px'} height={'110px'}/>
+      <nav className={`absolute mx-auto top-0 left-0 right-0 z-10 bg-opacity-100  text-white ${navbar?'bg-black':'bg-transparent'} `}>
+      
+      <div className="justify-between px-2 mx-auto lg:max-w-7xl md:items-center md:flex md:px-2">
+        <div>
+          <div className="flex items-center justify-between  md:py-0 md:block">
+            <Link href="/">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-red-700">
+                <img src="/logo.png" width={'100px'} height={'100px'}/>
               </span>
-            </h1>
-          </Link>
-  
-          <div className="mobile-menu block md:hidden">
-            {!navbarOpen ? (
+            </Link>
+            <div className="md:hidden">
               <button
-                onClick={() => setNavbarOpen(true)}
-                className="flex items-center px-3 py-1 border  rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+                className="p-2  rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}
               >
-               <GiHamburgerMenu/>
+                {navbar ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
               </button>
-            ) : (
-              <button
-                onClick={() => setNavbarOpen(false)}
-                className="flex items-center px-3 py-1 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-              >
-                <ImCross/>
-              </button>
-            )}
+            </div>
           </div>
-          <div className="menu hidden md:block md:w-auto " id="navbar">
-            <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0 ">
+        </div>
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            } `}
+          >
+            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              {
+                navLinks.map((item=>(
+                  <Link href={item.path} ><li className='my-3 px-2 py-2 hover:bg-white hover:text-black'>
+                    {item.year?<>
+                      <button className={'flex justify items-center'}onClick={()=>setOpen(!open)}>{item.title}<span><IoIosArrowDown/></span></button>
+                      <div className={`${open?'block':'hidden'} transition-all duration-6000 ease-in-out xl:absolute top-19.5 bg-white rounded rounded-5 z-10   px-[20px] py-[10px]`}>
+                
+                <ul className='transition-all duration-300 ease-in-out'>
+                  {
+                    item.year.map((item)=>(
+                      <Link 
+                      href={{
+                        pathname: "/Events",
+                        query: { year: `${item}` },
+                      }}
+                      
+                      ><li onClick={()=>setOpen(false)}className="text-black my-2 hover:bg-gray-500 hover:text-white pr-5 pl-2">{item}</li></Link>
 
-              {/* <li className='z-10 text-white relative'>
-                <button onClick={()=>{setOpen(!open)}}>Events</button>
-                <div className={`${open?'show':'hidden'} absolute top-10` }>
-                  <ul>
-                    <li>2017</li>
-                    <li>2018</li>
-                    <li>2019</li>
+                    ))
+                  }
+
+
+                </ul>
+              </div>
+                    
+                    </>
+                    :item.title}
+                  </li></Link>
+                )))
+              }
+              {/* <li className="hover:text-blue-600">
+                <Link href="">Home</Link>
+              </li>
+              <li className=" hover:text-blue-600">
+                <Link href="">Blog</Link>
+              </li>
+              <li>
+              <button className={'flex justify items-center'}onClick={()=>setOpen(!open)}>News<span><IoIosArrowDown/></span></button>
+                <div className={`${open?'block':'hidden'} xl:absolute top-19.5 bg-white rounded rounded-5 z-10   px-[20px] py-[10px]`}>
+                
+                  <ul >
+                    {
+                      cat.map((item)=>(
+                        <Link href={`/news/${item}`}><li onClick={()=>setOpen(false)}className="text-black my-2 hover:bg-gray-500 hover:text-white pr-5 pl-2">{item}</li></Link>
+
+                      ))
+                    }
+
+
                   </ul>
                 </div>
+              </li>
+              <li className="hover:text-blue-600">
+                <Link href="about">About US</Link>
+              </li>
+              <li className=" hover:text-blue-600">
+                <Link href="/contact">Contact US</Link>
               </li> */}
-              {navLinks.map((link, index) => (
-                <li key={index} onClick={()=>{setActive(link.id)}} >
-                 
-
-                  <NavLinks link={link} active={active} />
-                </li>
-              ))}
             </ul>
           </div>
         </div>
-        {navbarOpen ? <MenuOverLay  /> : null}
-      </nav>
+      </div>
+    </nav>
+   
   
 
 
